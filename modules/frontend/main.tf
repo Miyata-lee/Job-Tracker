69f97ffa642ae94be2263c7843af5a58f1d169ac
Miyata-lee <ashiqmeeran786@gmail.com>
2025-11-01 13:39:53 +0530

    Infra Deployment

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
